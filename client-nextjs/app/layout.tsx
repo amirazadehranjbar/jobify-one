190dfe67103dd2048aac3852a7fc70988a5782c2
Amir Azadeh Ranjbar <78546538+amirazadehranjbar@users.noreply.github.com>
2026-07-28 06:48:45 -0700

    add components

---
+++ client-nextjs/app/layout.tsx
@@ -0,0 +1,35 @@
+import type {Metadata} from "next";
+import "./globals.css";
+import {TooltipProvider} from "@/components/ui/tooltip"
+import React from "react";
+import {ThemeProvider} from "@/components/theme-provider";
+
+export const metadata: Metadata = {
+    title: "Jobify",
+    description: "find your job",
+};
+
+export default function RootLayout({
+                                       children,
+                                   }: Readonly<{
+    children: React.ReactNode;
+}>) {
+    return (
+        <html
+            lang="en"
+            className="h-full antialiased"
+        >
+        <body className="min-h-full flex flex-col">
+        <TooltipProvider>
+            <ThemeProvider
+                attribute="class"
+                defaultTheme="system"
+                enableSystem
+                disableTransitionOnChange
+            >
+                {children}
+            </ThemeProvider></TooltipProvider>
+        </body>
+        </html>
+    );
+}
